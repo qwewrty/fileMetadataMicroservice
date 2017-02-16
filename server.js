f299a9093d09758489dd67ba82fc0ebeda6d9b1f
qazxswedc <sandeep.carmel@gmail.com>
2017-02-16 07:27:35 +0000

    still not working

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
 var express = require("express");
 var app = express(); 
+var multer  = require('multer')
+var upload = multer({ dest: './uploads/' })
+
+//For getting the port number.
+app.set('port', (process.env.PORT || 5000));
+
+//requied to fetch static pages.
+app.use(express.static('.'));
+
+app.get("/", function(req, res){
+    res.send('index.html');
+} )
+
+app.post("/", upload.single('myFile'), function(req, res, next){
+    console.log(req.file);
+    //res.send(req.file)
+})
+
+//Listen on a given port.
+app.listen(app.get('port'), function(){
+    console.log('listening on :'+ app.get('port'))
+});
